4b2c043b7a4db489fda5f78ba3d6658c7a56a116
Gourav Chouhan <gouravchouhan156@gmail.com>
2026-04-29 14:29:45 +0530

    fix api

--- bytebuzz-frontend/src/services/RoomService.js
+++ bytebuzz-frontend/src/services/RoomService.js
@@ -1,22 +1,21 @@
 import { httpClient } from "../config/AxiosHelper";
 
+// ✅ Create Room
 export const createRoomApi = async (roomDetail) => {
-  const respone = await httpClient.post(`/api/v1/rooms`, roomDetail, {
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
-  return respone.data;
-};
-
-export const joinChatApi = async (roomId) => {
-  const response = await httpClient.get(`/api/v1/rooms/${roomId}`);
+  const response = await httpClient.post("/api/rooms", roomDetail);
   return response.data;
 };
 
+// ✅ Join Room
+export const joinChatApi = async (roomId) => {
+  const response = await httpClient.get(`/api/rooms/${roomId}`);
+  return response.data;
+};
+
+// ✅ Get Messages
 export const getMessagess = async (roomId, size = 50, page = 0) => {
   const response = await httpClient.get(
-    `/api/v1/rooms/${roomId}/messages?size=${size}&page=${page}`
+    `/api/rooms/${roomId}/messages?size=${size}&page=${page}`
   );
   return response.data;
 };
