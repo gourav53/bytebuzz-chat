e87e7a8cdf73e8c7d1eda4f13a6777890e9f3046
Gourav Chouhan <gouravchouhan156@gmail.com>
2026-04-30 12:46:48 +0530

    fix: timezone issue IST

--- bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/entities/Message.java
+++ bytebuzz-backend/src/main/java/com/rachit/bytebuzz/backend/entities/Message.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 @Getter
 @Setter
@@ -16,12 +17,11 @@ public class Message
 {
     private String sender;
     private String content;
-    private LocalDateTime timeStamp;
-
-    public Message(String sender, String content)
-    {
+    private OffsetDateTime timeStamp;
+-
+    public Message(String sender, String content) {
         this.sender = sender;
         this.content = content;
-        this.timeStamp = LocalDateTime.now();
+        this.timeStamp = OffsetDateTime.now(ZoneId.of("Asia/Kolkata"));
     }
 }
