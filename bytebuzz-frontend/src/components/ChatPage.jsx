682af4c58ca11fd264a3337be90b3811c6909ced
Gourav Chouhan <gouravchouhan156@gmail.com>
2026-04-30 12:24:23 +0530

    fixed time display

--- bytebuzz-frontend/src/components/ChatPage.jsx
+++ bytebuzz-frontend/src/components/ChatPage.jsx
@@ -164,7 +164,10 @@ const ChatPage = () => {
                 </p>
 
                 <p className="text-xs opacity-70 mt-1">
-                  {timeAgo(msg.timeStamp)}
+                  {new Date(msg.timeStamp).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
                 </p>
               </div>
             </div>
